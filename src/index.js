f62c6e22572466af9f14061d9c14821b6577ea19
Extazykeep <52160853+Extazykeep@users.noreply.github.com>
2021-03-13 16:42:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketpairs = [];
+  for(let i =0;i<bracketsConfig.length;i++){
+    bracketpairs.push(bracketsConfig[i][0]+bracketsConfig[i][1])
+  }
+  for(let i = 0; i < str.length;){
+    if(str.includes(bracketpairs[i])){
+      str = str.replace(bracketpairs[i],"");
+      i = 0;
+    }
+   else i++
+  }
+  return str.length === 0 
 }
